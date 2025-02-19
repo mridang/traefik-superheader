64e7f1a75871ed4b3df5155bcfa78e790526a009
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-02-19 14:31:09 +0700

    Modularised the handlers even further for better maintainability

--- demo.go
+++ demo.go
@@ -2,7 +2,6 @@ package superheader
 
 import (
 	"context"
-	"log"
 	"net/http"
 )
 
@@ -50,7 +49,7 @@ type Demo struct {
 }
 
 // New created a new Demo plugin.
-func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	return &Demo{
 		next:   next,
 		name:   name,
@@ -58,152 +57,10 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 	}, nil
 }
 
-func (sh *Demo) LogMessage(headerKey string, headerValue string) {
-	log.Printf("Warning: Incorrect configuration for header '%s'. '%s' is not a valid value. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/%s", headerKey, headerValue, headerKey)
-}
-
 func (sh *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	switch sh.config.XFrameOptions {
-	case
-		"DENY",
-		"SAMEORIGIN":
-		rw.Header().Set("X-Frame-Options", sh.config.XFrameOptions)
-	default:
-		sh.LogMessage("X-Frame-Options", sh.config.XFrameOptions)
-	}
-
-	// X-DNS-Prefetch-Control
-	switch sh.config.XDNSPrefetchControl {
-	case
-		"on",
-		"off":
-		rw.Header().Set("X-DNS-Prefetch-Control", sh.config.XDNSPrefetchControl)
-	default:
-		sh.LogMessage("X-DNS-Prefetch-Control", sh.config.XDNSPrefetchControl)
-	}
-
-	// X-Content-Type-Options
-	switch sh.config.XContentTypeOptions {
-	case
-		"on":
-		rw.Header().Set("X-Content-Type-Options", "nosniff")
-	case
-		"off":
-		// Skip setting the header
-	default:
-		sh.LogMessage("X-Content-Type-Options", sh.config.XDNSPrefetchControl)
-	}
-
-	// Strict-Transport-Security
-	switch sh.config.StrictTransportSecurity {
-	case
-		"on":
-		rw.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
-	case
-		"off":
-		// Skip setting the header
-	default:
-		sh.LogMessage("Strict-Transport-Security", sh.config.XDNSPrefetchControl)
-	}
-
-	// Referrer-Policy
-	switch sh.config.ReferrerPolicy {
-	case
-		"no-referrer",
-		"no-referrer-when-downgrade",
-		"origin",
-		"origin-when-cross-origin",
-		"same-origin",
-		"strict-origin",
-		"strict-origin-when-cross-origin",
-		"unsafe-url":
-		rw.Header().Set("Referrer-Policy", sh.config.ReferrerPolicy)
-	case "off":
-		// Skip setting the header
-	default:
-		sh.LogMessage("Referrer-Policy", sh.config.XDNSPrefetchControl)
-	}
-
-	// X-XSS-Protection
-	switch sh.config.XXSSProtection {
-	case
-		"on":
-		rw.Header().Set("X-XSS-Protection", "1")
-	case
-		"block":
-		rw.Header().Set("X-XSS-Protection", "1; mode=block")
-	case
-		"off":
-		rw.Header().Set("X-XSS-Protection", "0")
-	default:
-		sh.LogMessage("X-XSS-Protection", sh.config.XDNSPrefetchControl)
-	}
-
-	// Cross-Origin-Opener-Policy
-	switch sh.config.CrossOriginOpenerPolicy {
-	case
-		"unsafe-none",
-		"same-origin-allow-popups",
-		"same-origin",
-		"noopener-allow-popups":
-		rw.Header().Set("Cross-Origin-Opener-Policy", sh.config.CrossOriginOpenerPolicy)
-	case "off":
-		// Skip setting the header
-	default:
-		sh.LogMessage("Cross-Origin-Opener-Policy", sh.config.XDNSPrefetchControl)
-	}
-
-	// Cross-Origin-Embedder-Policy
-	switch sh.config.CrossOriginEmbedderPolicy {
-	case
-		"unsafe-none",
-		"require-corp",
-		"credentialless":
-		rw.Header().Set("Cross-Origin-Embedder-Policy", sh.config.CrossOriginEmbedderPolicy)
-	case "off":
-		// Skip setting the header
-	default:
-		sh.LogMessage("Cross-Origin-Embedder-Policy", sh.config.CrossOriginEmbedderPolicy)
-	}
-
-	switch sh.config.CrossOriginResourcePolicy {
-	case
-		"same-origin",
-		"same-site",
-		"cross-origin":
-		rw.Header().Set("Cross-Origin-Resource-Policy", sh.config.CrossOriginResourcePolicy)
-	case "off":
-		// Skip setting the header
-	default:
-		sh.LogMessage("Cross-Origin-Resource-Policy", sh.config.CrossOriginResourcePolicy)
-	}
-
-	switch sh.config.OriginAgentCluster {
-	case
-		"on":
-		rw.Header().Set("Origin-Agent-Cluster", "?1")
-	case "off":
-		// Skip setting the header
-	default:
-		sh.LogMessage("Origin-Agent-Cluster", sh.config.OriginAgentCluster)
-	}
-
-	switch sh.config.XPermittedCrossDomainPolicies {
-	case
-		"none",
-		"master-only",
-		"by-content-type",
-		"by-ftp-filename",
-		"all",
-		"none-this-response":
-		rw.Header().Set("X-Permitted-Cross-Domain-Policies", sh.config.XPermittedCrossDomainPolicies)
-	case "off":
-		// Skip setting the header
-	default:
-		sh.LogMessage("X-Permitted-Cross-Domain-Policies", sh.config.XPermittedCrossDomainPolicies)
-	}
-
 	sh.next.ServeHTTP(rw, req)
+
+	AddSecureHeaders(sh.config, rw, req)
 
 	switch sh.config.RemovePoweredBy {
 	case "on":
---
+++ secure_headers.go
@@ -0,0 +1,152 @@
+package superheader
+
+import (
+	"log"
+	"net/http"
+)
+
+func LogMessage(headerKey string, headerValue string) {
+	log.Printf("Warning: Incorrect configuration for header '%s'. '%s' is not a valid value. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/%s", headerKey, headerValue, headerKey)
+}
+
+func AddSecureHeaders(config *Config, rw http.ResponseWriter, _ *http.Request) {
+	switch config.XFrameOptions {
+	case
+		"DENY",
+		"SAMEORIGIN":
+		rw.Header().Set("X-Frame-Options", config.XFrameOptions)
+	default:
+		LogMessage("X-Frame-Options", config.XFrameOptions)
+	}
+
+	// X-DNS-Prefetch-Control
+	switch config.XDNSPrefetchControl {
+	case
+		"on",
+		"off":
+		rw.Header().Set("X-DNS-Prefetch-Control", config.XDNSPrefetchControl)
+	default:
+		LogMessage("X-DNS-Prefetch-Control", config.XDNSPrefetchControl)
+	}
+
+	// X-Content-Type-Options
+	switch config.XContentTypeOptions {
+	case
+		"on":
+		rw.Header().Set("X-Content-Type-Options", "nosniff")
+	case
+		"off":
+		// Skip setting the header
+	default:
+		LogMessage("X-Content-Type-Options", config.XDNSPrefetchControl)
+	}
+
+	// Strict-Transport-Security
+	switch config.StrictTransportSecurity {
+	case
+		"on":
+		rw.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+	case
+		"off":
+		// Skip setting the header
+	default:
+		LogMessage("Strict-Transport-Security", config.XDNSPrefetchControl)
+	}
+
+	// Referrer-Policy
+	switch config.ReferrerPolicy {
+	case
+		"no-referrer",
+		"no-referrer-when-downgrade",
+		"origin",
+		"origin-when-cross-origin",
+		"same-origin",
+		"strict-origin",
+		"strict-origin-when-cross-origin",
+		"unsafe-url":
+		rw.Header().Set("Referrer-Policy", config.ReferrerPolicy)
+	case "off":
+		// Skip setting the header
+	default:
+		LogMessage("Referrer-Policy", config.XDNSPrefetchControl)
+	}
+
+	// X-XSS-Protection
+	switch config.XXSSProtection {
+	case
+		"on":
+		rw.Header().Set("X-XSS-Protection", "1")
+	case
+		"block":
+		rw.Header().Set("X-XSS-Protection", "1; mode=block")
+	case
+		"off":
+		rw.Header().Set("X-XSS-Protection", "0")
+	default:
+		LogMessage("X-XSS-Protection", config.XDNSPrefetchControl)
+	}
+
+	// Cross-Origin-Opener-Policy
+	switch config.CrossOriginOpenerPolicy {
+	case
+		"unsafe-none",
+		"same-origin-allow-popups",
+		"same-origin",
+		"noopener-allow-popups":
+		rw.Header().Set("Cross-Origin-Opener-Policy", config.CrossOriginOpenerPolicy)
+	case "off":
+		// Skip setting the header
+	default:
+		LogMessage("Cross-Origin-Opener-Policy", config.XDNSPrefetchControl)
+	}
+
+	// Cross-Origin-Embedder-Policy
+	switch config.CrossOriginEmbedderPolicy {
+	case
+		"unsafe-none",
+		"require-corp",
+		"credentialless":
+		rw.Header().Set("Cross-Origin-Embedder-Policy", config.CrossOriginEmbedderPolicy)
+	case "off":
+		// Skip setting the header
+	default:
+		LogMessage("Cross-Origin-Embedder-Policy", config.CrossOriginEmbedderPolicy)
+	}
+
+	switch config.CrossOriginResourcePolicy {
+	case
+		"same-origin",
+		"same-site",
+		"cross-origin":
+		rw.Header().Set("Cross-Origin-Resource-Policy", config.CrossOriginResourcePolicy)
+	case "off":
+		// Skip setting the header
+	default:
+		LogMessage("Cross-Origin-Resource-Policy", config.CrossOriginResourcePolicy)
+	}
+
+	switch config.OriginAgentCluster {
+	case
+		"on":
+		rw.Header().Set("Origin-Agent-Cluster", "?1")
+	case "off":
+		// Skip setting the header
+	default:
+		LogMessage("Origin-Agent-Cluster", config.OriginAgentCluster)
+	}
+
+	switch config.XPermittedCrossDomainPolicies {
+	case
+		"none",
+		"master-only",
+		"by-content-type",
+		"by-ftp-filename",
+		"all",
+		"none-this-response":
+		rw.Header().Set("X-Permitted-Cross-Domain-Policies", config.XPermittedCrossDomainPolicies)
+	case "off":
+		// Skip setting the header
+	default:
+		LogMessage("X-Permitted-Cross-Domain-Policies", config.XPermittedCrossDomainPolicies)
+	}
+}
